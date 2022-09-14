bfdf80e9cdd359bba50ff41319bca70276639d03
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-09-14 09:13:58 +0200

    Revert "Initial commit"
    This reverts commit 2352e9fb8a95acd5dc865a04d57b5352071cd864.

--- app.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import datetime as dt
-import os
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
