f136836a87769d25b48d24e367a2e1f32c87341d
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-09-15 17:54:56 +0200

    Add callbacks

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+import dash_auth
+import plotly.graph_objects as go
+import tab1
+import tab2
+
 
 class db:
     def __init__(self):
@@ -55,6 +60,58 @@ app.layout = html.Div([html.Div([dcc.Tabs(id='tabs', value='tab-1', children=[
     html.Div(id='tabs-content')],
     style={'width':'80%', 'margin':'auto'})],
     style={'height':'100%'})
+
+
+
+@app.callback(Output('tabs-content', 'children'), [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return tab1.render_tab(df.merged)
+    elif tab == 'tab-2':
+        return tab2.render_tab(df.merged)
+
+@app.callback(Output('bar-sales', 'figure'),
+    [Input('sales-range', 'start_date'),Input('sales-range', 'end_date')])
+def tab1_bar_sales(start_date, end_date):
+    truncated = df.merged[(df.merged['tran_date']>=start_date) & (df.merged['tran_date']<=end_date)]
+    grouped = truncated[truncated['total_amt']>0].groupby([pd.Grouper(key='tran_date', freq='M'), 'Stgore_type'])['total_amt'].sum().round(2).unstack()
+
+    traces = []
+    for col in grouped.columns:
+        traces.append(go.Bar(x=grouped.index, y=grouped[col], name=col, hoverinfo='text',
+                        hovertext=[f'{y/1e3:.2f}k' for y in grouped[col].values]))
+
+    data = traces
+    fig = go.Figure(data=data, layout=go.Layout(title='Przychody', barmode='stack', legend=dict(x=0, y=-0.5)))        
+    return fig
+
+@app.callback(Output('choropleth-sales', 'figure'),
+            [Input('sales-range', 'start_date'), Input('sales-range', 'end-date')])
+def tab1_choropleth_sales(start_date, end_date):
+    truncated = df.merged[(df.merged['tran_date']>=start_date)&(df.merged['tran_date']<=end_date)]
+    grouped = truncated[truncated['total_amt']>0].groupby('country')['total_amt'].sum().round(2)
+
+    trace0 = go.Choropleth(colorscale='Viridis',reversescale=True,
+                            locations=grouped.index,locationmode='country names',
+                            z = grouped.values, colorbar=dict(title='Sales'))
+    data = [trace0]
+    fig = go.Figure(data=data,layout=go.Layout(title='Mapa',geo=dict(showframe=False,projection={'type':'natural earth'})))
+
+    return fig
+
+@app.callback(Output('barh-prod-subcat','figure'),
+            [Input('prod_dropdown','value')])
+def tab2_barh_prod_subcat(chosen_cat):
+
+    grouped = df.merged[(df.merged['total_amt']>0)&(df.merged['prod_cat']==chosen_cat)].pivot_table(index='prod_subcat',columns='Gender',values='total_amt',aggfunc='sum').assign(_sum=lambda x: x['F']+x['M']).sort_values(by='_sum').round(2)
+
+    traces = []
+    for col in ['F','M']:
+        traces.append(go.Bar(x=grouped[col],y=grouped.index,orientation='h',name=col))
+
+    data = traces
+    fig = go.Figure(data=data,layout=go.Layout(barmode='stack',margin={'t':20,}))
+    return fig
 
 
 if __name__ == '__main__':
