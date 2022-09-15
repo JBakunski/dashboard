4b48e6b7407e5aa722c50acc7cef39a5be7dfe82
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-09-15 19:19:14 +0200

    Add to tab3 sales per store type and gender

--- tab3.py
+++ tab3.py
@@ -9,11 +9,11 @@ def render_tab(df):
 
 
     layout = html.Div([html.H1('Kanały sprzedaży', style={'text-align':'center'}),
-                html.Div([html.Div([dcc.Graph(id='pie-prod-cat', figure=fig)],
+                html.Div([html.Div([dcc.Graph(id='pie-store-type', figure=fig)],
                                     style={'width':'50%'}),
-                            # html.Div([dcc.Dropdown(id='store_dropdown', options=[{'label':store_type, 'value':store_type} for store_type in df['store_type'].unique()], value=df['store_type'].unique()[0]),
-                            # dcc.Graph(id='barh-prod-subcat')], style={'width':'50%'})], style={'display':'flex'}),
+                            html.Div([dcc.Dropdown(id='store_dropdown', options=[{'label':store_type, 'value':store_type} for store_type in df['Store_type'].unique()], value=df['Store_type'].unique()[0]),
+                            dcc.Graph(id='barh-store-subcat')], style={'width':'50%'})], style={'display':'flex'}),
                             html.Div(id='temp-out')
-                            ])])
+                            ])
     
     return layout
