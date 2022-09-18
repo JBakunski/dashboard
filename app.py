765bc3f82ddbe08c1b770599a96f791278852731
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-09-18 12:31:00 +0200

    Refactor code

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import datetime as dt
-import os
 import dash
 from dash import dcc
 from dash import html
@@ -10,43 +8,9 @@ import plotly.graph_objects as go
 import tab1
 import tab2
 import tab3
+import db
 
 
-class db:
-    def __init__(self):
-        self.transactions = db.transaction_init()
-        self.cc = pd.read_csv(r'db\country_codes.csv', index_col=0)
-        self.customers = pd.read_csv(r'db\customers.csv', index_col=0)
-        self.prod_info = pd.read_csv(r'db\prod_cat_info.csv')
-    
-    @staticmethod
-    def transaction_init():
-        transactions = pd.DataFrame()
-        src = r'db\transactions'
-        for filename in os.listdir(src):
-            transactions = pd.concat([transactions, pd.read_csv(os.path.join(src, filename))])
-        
-        def convert_dates(x):
-            try:
-                return dt.datetime.strptime(x, '%d-%m-%Y')
-            except:
-                return dt.datetime.strptime(x, '%d/%m/%Y')
-        
-        transactions['tran_date'] = transactions['tran_date'].apply(lambda x: convert_dates(x))
-
-        return transactions
-    
-    def merge(self):
-        df = self.transactions.join(self.prod_info.drop_duplicates(subset=['prod_cat_code'])
-        .set_index('prod_cat_code')['prod_cat'], on='prod_cat_code', how='left')
-
-        df = df.join(self.prod_info.drop_duplicates(subset=['prod_sub_cat_code'])
-        .set_index('prod_sub_cat_code')['prod_subcat'], on='prod_subcat_code', how='left')
-
-        df = df.join(self.customers.join(self.cc, on='country_code')
-        .set_index('customer_Id'), on='cust_id')
-
-        self.merged = df
 
 df = db()
 df.merge()
@@ -57,6 +21,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD)
 
+# General layout
 app.layout = html.Div([html.Div([dcc.Tabs(id='tabs', value='tab-1', children=[
     dcc.Tab(label='Sprzedaż globalna', value='tab-1'),
     dcc.Tab(label='Produkty', value='tab-2'),
@@ -77,6 +42,7 @@ def render_content(tab):
     elif tab == 'tab-3':
         return tab3.render_tab(df.merged)
 
+# tab1 callbacks
 @app.callback(Output('bar-sales', 'figure'),
     [Input('sales-range', 'start_date'),Input('sales-range', 'end_date')])
 def tab1_bar_sales(start_date, end_date):
@@ -107,7 +73,7 @@ def tab1_choropleth_sales(start_date,end_date):
 
     return fig
 
-
+# tab2 callbacks
 @app.callback(Output('barh-prod-subcat','figure'),
             [Input('prod_dropdown','value')])
 def tab2_barh_prod_subcat(chosen_cat):
--- tab3.py
+++ tab3.py
@@ -3,20 +3,7 @@ from dash import dcc
 from dash import html
 
 def render_tab(df):
-    # grouped = df[df['total_amt']>0].groupby('tran_date')['total_amt'].sum()
-    # labels = grouped.index.weekday.sort_values().map({0: 'Poniedziałek',
-    #                                                           1: 'Wtorek',
-    #                                                           2: 'Środa',
-    #                                                           3: 'Czwartek',
-    #                                                           4: 'Piątek',
-    #                                                           5: 'Sobota',
-    #                                                           6: 'Niedziela'})
-                                
-    # fig = go.Figure(data=[go.Pie(labels=labels, values=grouped.values, sort=False)], layout=go.Layout(title='Udział poszczególnych kanałów w sprzedaży'))
-
-    # print(grouped.head())
-    # print(labels)
-
+    
     layout = html.Div([html.H1('Kanały sprzedaży', style={'text-align':'center'}),
                 html.Div([html.Div([dcc.Graph(id='pie-weekday-sales')],
                                     style={'width':'50%'}),
